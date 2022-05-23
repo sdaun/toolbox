18b2477b7e19aed2d02a24983e6fde8aca849bfb
Sabrina Daun <sabrinadaun@gmx.de>
2022-05-23 15:53:53 +0200

    Added scraping to scripts

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='toolbox',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/toolbox-run'],
+      scripts=['scripts/toolbox-run', 'scripts/toolbox-scraping'],
       zip_safe=False)
